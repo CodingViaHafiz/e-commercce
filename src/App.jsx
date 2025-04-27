fb814f880e2b18a705dac0f732c77f0662ecc299
Mohsin Iqbal <mohsiniqbalbhatti0024@gmail.com>
2025-04-27 20:58:46 +0500

    e-commerce website

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./components/Home.jsx";
+import "./index.css"; // ✅ Make sure the path is correct
+
+function App() {
+  return <Home />;
+}
+
+export default App;
